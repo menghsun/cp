daa34689a13027c173a18223539199ffb035bae4
menghsun <tsaimh@gmail.com>
2020-02-05 14:27:34 +0800

    add lec01/hello.cpp

---
+++ lec01/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>    // for using cout
+using namespace std;   // avoid repeating “std::”
+
+int main()
+{
+	std::cout << "Hello ";
+	cout << "NC" << "KU!!" << endl;
+	return 0;
+}
+
