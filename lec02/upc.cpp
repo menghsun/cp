00f5868bc48ae5a75090b709d0681f6bcd16942f
menghsun <tsaimh@gmail.com>
2020-03-18 16:29:03 +0800

    shorten printed message

--- lec02/upc.cpp
+++ lec02/upc.cpp
@@ -7,15 +7,15 @@ int main()
 	int d, first_sum, second_sum, total;
 	string first_group, second_group;
 
-	cout << "Enter the first (single) digit: ";
+	cout << "First digit: ";
 	cin >> d;
-	cout << "Enter first group of five digits: ";
+	cout << "1st group (5 digits): ";
 	cin >> first_group;
-	cout << "Enter second group of five digits: ";
+	cout << "2nd group (5 digits): ";
 	cin >> second_group;
 
-	first_sum = d + (first_group[1]-48) + (first_group[3]-48) + (second_group[0]-48) + (second_group[2]-48) + (second_group[4]-48);
-	second_sum =  (first_group[0]-48) + (first_group[2]-48) + (first_group[4]-48) + (second_group[1]-48) + (second_group[3]-48);
+	first_sum = d + (first_group[1]-'0') + (first_group[3]-'0') + (second_group[0]-'0') + (second_group[2]-'0') + (second_group[4]-'0');
+	second_sum =  (first_group[0]-'0') + (first_group[2]-'0') + (first_group[4]-'0') + (second_group[1]-'0') + (second_group[3]-'0');
 	total = 3 * first_sum + second_sum;
 
 	cout << "Check digit: " << 9 - ((total - 1) % 10) << "\n";
